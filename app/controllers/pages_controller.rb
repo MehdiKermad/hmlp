a70506101743fcf4dd8f1f6cb28d0a7f720585f4
Mehdi <mkermad@live.fr>
2017-06-05 19:38:27 +0200

    ajout du tableau

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,9 @@
 class PagesController < ApplicationController
   def home
-    @variable = ["a","b","c"]
+    
   end
 
   def results
-    @variable = "bonjour"
-  end
-
-  def search
     
     # on vérifie que l'on dispose de toute les données
     if(params[:email].blank? || params[:lat].nil? || params[:lng].nil?)
@@ -23,7 +19,7 @@ class PagesController < ApplicationController
       # on effectue la recherche sur 1 km
       #occurences = Query.where :lat => params[:lat], :lng => params[:lng], :with => {:geodist => 0.0..1_000.0}
 
-      occurences = [1, 2, 3, 4]
+      @occurences = Transaction.all
 
 
       # une fois les occurences récupérées, on enregistre la requete dans la table Query
@@ -31,10 +27,8 @@ class PagesController < ApplicationController
       query.email = params[:email]
       query.lat = params[:lat]
       query.lng = params[:lng]
-      query.result_count = occurences.length
+      query.result_count = @occurences.length
       query.save
-
-      redirect_to "/results"
     end
   end
 end
